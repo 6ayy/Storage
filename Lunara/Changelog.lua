1bef3685acd5e0f19318d7f53a6dd174e36eca75
fayy <101954998+babyfayy333@users.noreply.github.com>
2024-07-07 21:25:07 +1200

    Create Changelog.lua

---
+++ Lunara/Changelog.lua
@@ -0,0 +1,12 @@
+local Changelog = {
+  ["Ultimate"] = {
+    Version = "0.0.0.0",
+    Text = "- Not public released"
+  },
+  ["Lite"] = {
+    Version = "0.0.1.2",
+    Text = "- Fixed pathfinding\n- Fixed stat labels"
+  }
+}
+
+return Changelog
