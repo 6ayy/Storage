46a378be642599c90758e43b30198fe251527122
fayy <101954998+babyfayy333@users.noreply.github.com>
2024-07-07 21:33:01 +1200

    Create Developers.lua

---
+++ Lunara/Developers.lua
@@ -0,0 +1,16 @@
+local Developers = {
+    ["Babyfayy333"] = {
+        Name = "Babyfayy333",
+        Discord = "https://discordapp.com/users/855840079376285727",
+    },
+    ["Gladiator"] = {
+        Name = "Gladiator_0315",
+        Discord = "https://discordapp.com/users/1004994479482228747",
+    },
+    ["Blitz"] = {
+        Name = "Blitz",
+        Discord = "https://discordapp.com/users/576849921471152140",
+    }
+}
+
+return Developers
