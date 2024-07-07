65465ac6d22f8271868a1b78a34a8e5773a9b6df
fayy <101954998+babyfayy333@users.noreply.github.com>
2024-07-07 21:28:56 +1200

    Create ArrestMessages.lua

---
+++ Lunara/ArrestMessages.lua
@@ -0,0 +1,15 @@
+local Messages = {
+    -- jailbroke autoarrest messages (ill encrypt these soon :yawn:)
+    "Someone pls fix jailbreak anticheat (it's while true do end)",
+    "Get Lunara at lunara.fayy.xyz / ",
+    "Best auto farm for jailbreak @ lunara.fayy.xyz!!!!",
+    "This Auto Arrest Was Unpatched By Babyfayy333",
+    "FLYING CAMARO FLYING CAMARO FLYING CAMARO",
+    "Babyfayy333 is a good dev ngl",
+    "ngl your gonna get autoarrested rn",
+    "join up rn @ lunara.fayy.xyz",
+
+    -- universal farm autoarrest messages redacted for privacy :)
+}
+
+return Messages
